a646838cea55e97d25c744899eeb1d8c255cabdd
Me! <AliceAlexandriA@Alexandrias-MacBook-Air.local>
2013-01-25 10:15:32 -0600

    Initial Commit

---
+++ SumOfSevens/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SumOfSevens
+//
+//  Created by Me! on 1/25/13.
+//  Copyright (c) 2013 Me!. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
